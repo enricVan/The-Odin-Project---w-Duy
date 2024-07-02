fd2ca11522cfffb1b1d7a1ba299b9d7c36abaa78
EnricVan <vanminhtuan2003@gmail.com>
2024-07-02 15:46:18 +0700

    Buoi 16

---
+++ FoundationsCourse/B16-JavaScript/js/script.js
@@ -0,0 +1,50 @@
+// console.log("Hello World!");
+
+//Dùng var sẽ bị hoisting
+// console.log(myName);
+// var myName = "John";
+
+// Kiểu dữ liệu trong JavaScript: null, undefined, boolean, number, string, object {}, symbol
+// var myName;
+// console.log(myName); //undefined
+// myName = "John";
+
+// Trong ES6 phát triển JS sử dụng 2 cách khai báo mới sử dụng let và const (tránh bị hoisting)
+// let myName = "John";
+// myName = "Tuan";
+// console.log(myName);
+
+//Kiểu dữ liệu Number
+// let a = 10;
+// console.log(a);
+// console.log(9 * a);
+// let b = 7 * a;
+// console.log(b);
+
+//Kiểu dữ liệu String
+// let name = "Tuan";
+// let pattern = name.toUpperCase();
+// console.log(pattern);
+
+// Conditional Statements
+//Functions
+// function sayHello() {
+//   console.log("Hello World!");
+// }
+
+// //arrow function
+// const sayHello2 = () => {
+//   console.log("Hello World2!");
+// };
+
+// sayHello2();
+
+// const sum = (a, b) => {
+//   console.log("Tong la: ", a + b);
+// };
+
+// sum(5, 6);
+
+// const sum2 = (a, b) => a + b;
+
+// console.log("Tong la:", sum2(5, 6));
